ac56080433d9885d6e6f525f5fa2129b797820be
shynexiii <xiiihab@gmail.com>
2021-07-04 01:13:48 +0100

    Initial commit

---
+++ Nakirki_Crm/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
+    path('fournisseur/', include('provider.urls')),
+    path('produit/', include('product.urls')),
+]
